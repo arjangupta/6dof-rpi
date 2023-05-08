95ba4d7b911a575ab49f4731f4593a1c2aa0be6f
Arjan Gupta <arjangupta95@gmail.com>
2023-05-08 00:03:51 -0500

    Continue adjusting servo0 min/max pulse, angle range

--- controller.py
+++ controller.py
@@ -11,25 +11,13 @@ i2c = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c)
 pca.frequency = 50
 
-# To get the full range of the servo you will likely need to adjust the min_pulse and max_pulse to
-# match the stall points of the servo.
-# This is an example for the Sub-micro servo: https://www.adafruit.com/product/2201
-# servo7 = servo.Servo(pca.channels[7], min_pulse=580, max_pulse= 2350)
-# This is an example for the Micro Servo - High Powered, High Torque Metal Gear:
-#   https://www.adafruit.com/product/2307
-# servo7 = servo.Servo(pca.channels[7], min_pulse=500, max_pulse=2600)
-# This is an example for the Standard servo - TowerPro SG-5010 - 5010:
-#   https://www.adafruit.com/product/155
-# servo7 = servo.Servo(pca.channels[7], min_pulse=400, max_pulse=2400)
-# This is an example for the Analog Feedback Servo: https://www.adafruit.com/product/1404
-# servo7 = servo.Servo(pca.channels[7], min_pulse=600, max_pulse=2500)
-# This is an example for the Micro servo - TowerPro SG-92R: https://www.adafruit.com/product/169
-# servo7 = servo.Servo(pca.channels[7], min_pulse=500, max_pulse=2400)
-
 # The pulse range is 750 - 2250 by default. This range typically gives 135 degrees of
 # range, but the default is to use 180 degrees. You can specify the expected range if you wish:
 # servo7 = servo.Servo(pca.channels[7], actuation_range=135)
-servo0 = servo.Servo(pca.channels[0], actuation_range=135)
+servo0_range = 150
+servo0 = servo.Servo(pca.channels[0], actuation_range=servo0_range, min_pulse=500, max_pulse=2100)
+
+# Statically hold the rest of the servos
 servo1 = servo.Servo(pca.channels[1], actuation_range=135)
 servo2 = servo.Servo(pca.channels[2], actuation_range=135)
 servo3 = servo.Servo(pca.channels[3], actuation_range=135)
@@ -38,11 +26,11 @@ servo2.angle = 90
 servo3.angle = 90
 
 # We sleep in the loops to give the servo time to move into position.
-for i in range(135):
+for i in range(servo0_range):
     servo0.angle = i
     time.sleep(0.03)
-for i in range(135):
-    servo0.angle = 135 - i
+for i in range(servo0_range):
+    servo0.angle = servo0_range - i
     time.sleep(0.03)
 
 # # You can also specify the movement fractionally.
