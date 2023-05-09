6ecaa6de50bfbba77026d818c07fadc9f9ce865f
Arjan Gupta <arjangupta95@gmail.com>
2023-05-08 21:55:24 -0500

    Set all servos to callibrated settings, try sweeping servo #3

--- controller.py
+++ controller.py
@@ -12,25 +12,25 @@ pca = PCA9685(i2c)
 pca.frequency = 50
 
 # Set up for servo #0
-servo0_range = 180
-servo0 = servo.Servo(pca.channels[0], actuation_range=servo0_range, min_pulse=480, max_pulse=1970)
+servo_range = 180
+servo0 = servo.Servo(pca.channels[0], actuation_range=servo_range, min_pulse=480, max_pulse=1970)
 
 # Statically hold the rest of the servos
-servo1 = servo.Servo(pca.channels[1], actuation_range=135)
-servo2 = servo.Servo(pca.channels[2], actuation_range=135)
-servo3 = servo.Servo(pca.channels[3], actuation_range=135)
+servo1 = servo.Servo(pca.channels[1], actuation_range=servo_range, min_pulse=480, max_pulse=1970)
+servo2 = servo.Servo(pca.channels[2], actuation_range=servo_range, min_pulse=480, max_pulse=1970)
+servo3 = servo.Servo(pca.channels[3], actuation_range=servo_range, min_pulse=480, max_pulse=1970)
 servo1.angle = 100
 servo2.angle = 90
 servo3.angle = 90
 
 # We sleep in the loops to give the servo time to move into position.
-for i in range(servo0_range):
-    servo0.angle = i
+for i in range(servo_range):
+    servo3.angle = i
     time.sleep(0.03)
 # Wait before sweeping back
 time.sleep(10)
-for i in range(servo0_range):
-    servo0.angle = servo0_range - i
+for i in range(servo_range):
+    servo3.angle = servo_range - i
     time.sleep(0.03)
 
 # # You can also specify the movement fractionally.
