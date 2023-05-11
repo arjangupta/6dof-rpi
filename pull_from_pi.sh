a6845b1e0fcc17be886f15db6ec8ce6320df7bdb
Arjan Gupta <arjangupta95@gmail.com>
2023-05-10 22:37:49 -0500

    Edit the pull/push rpi scripts to use helper functions in them

--- pull_from_pi.sh
+++ pull_from_pi.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# This function takes in filename as an argument, and gets it via scp from the RPi
+function get_file_from_pi() {
+    scp pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/$1 ./$1
+
 # Get files from the pi
-scp pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/controller.py   ./controller.py
-scp pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/version.txt     ./version.txt
-scp pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/range-tester.py ./range-tester.py
+get_file_from_pi version.txt
+get_file_from_pi range-tester.py
--- push_to_pi.sh
+++ push_to_pi.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# This function takes in filename as an argument, and pushes it via scp to the RPi
+function push_file_to_pi() {
+    scp ./$1 pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/$1
+
 # Use scp to copy the file to the pi
-scp ./controller.py   pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/controller.py
-scp ./version.txt     pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/version.txt
-scp ./range-tester.py pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/range-tester.py
+push_file_to_pi version.txt
+push_file_to_pi range-tester.py
