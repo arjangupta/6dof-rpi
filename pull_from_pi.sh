e136552c43fe5acd52c2de364329a90d319a8fed
Arjan Gupta <arjangupta95@gmail.com>
2023-05-11 23:23:52 -0500

    Fix the push/pull scripts, increment version to 002

--- pull_from_pi.sh
+++ pull_from_pi.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # This function takes in filename as an argument, and gets it via scp from the RPi
-function get_file_from_pi() {
+get_file_from_pi() {
     scp pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/$1 ./$1
+}
 
 # Get files from the pi
 get_file_from_pi version.txt
--- push_to_pi.sh
+++ push_to_pi.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # This function takes in filename as an argument, and pushes it via scp to the RPi
-function push_file_to_pi() {
+push_file_to_pi() {
     scp ./$1 pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/$1
+}
 
 # Use scp to copy the file to the pi
 push_file_to_pi version.txt
