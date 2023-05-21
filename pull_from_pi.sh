6f69fabbd8c1d85c3448128219422fceecc75a2e
Arjan Gupta <arjangupta95@gmail.com>
2023-05-21 00:14:18 -0500

    Edit the pull/push rpi scripts for dirs, increment to v003

--- pull_from_pi.sh
+++ pull_from_pi.sh
@@ -5,7 +5,11 @@ get_file_from_pi() {
     scp pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/$1 ./$1
 }
 
+get_dir_from_pi() {
+    scp -r pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/$1 ./$1
+
 # Get files from the pi
 get_file_from_pi version.txt
 get_file_from_pi joint-tester.py
-get_file_from_pi robot-properties.json
+get_dir_from_pi server
+get_dir_from_pi sixdof_arm
--- push_to_pi.sh
+++ push_to_pi.sh
@@ -5,7 +5,12 @@ push_file_to_pi() {
     scp ./$1 pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/$1
 }
 
+push_dir_to_pi() {
+    scp -r ./$1 pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/$1
+}
+
 # Use scp to copy the file to the pi
 push_file_to_pi version.txt
 push_file_to_pi joint-tester.py
-push_file_to_pi robot-properties.json
+push_dir_to_pi server
+push_dir_to_pi sixdof_arm
