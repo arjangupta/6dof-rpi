91d7a6f1ee9678f271623e586ea3132727ea2c18
Arjan Gupta <arjangupta95@gmail.com>
2023-05-21 16:54:05 -0500

    Stop using overall project version as a txt

--- server/server.py
+++ server/server.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from sixdof_arm import Robot
-import os
+
+PROJECT_VERSION = "0.0.6"
 
 app = Flask(__name__)
 robot = Robot()
 
 @app.route('/')
 def display_welcome_message():
-    return "<p>You have found the 6 DoF robot backend server!</p><p>Use the client app to interact with this server meaningfully.<p>"
+    return f"<p>You have found the 6 DoF robot backend server. Overall project version is {PROJECT_VERSION}.</p><p>Use the client app to interact with this server meaningfully.<p>"
 
 """
 This endpoint is used to move the robot arm to a target angles of each joint.
@@ -18,24 +19,10 @@ This endpoint is used to move the robot arm to a target angles of each joint.
 def move_to():
     pass
 
-"""
-Route and helper function to show version number of system.
-Open the version.txt file and return the contents. If it doesn't exist, notify, show current directory and exit.
-"""
 @app.route('/version')
 def show_version():
-    try:
-        with open('version.txt') as f:
-            version = f.read()
-    except FileNotFoundError:
-        print("version.txt not found in current directory.")
-        print("Current directory is:")
-        print(os.getcwd())
-        exit()
-    return version
+    return f"<p>Overall project version is {PROJECT_VERSION}.</p>"
 
 if __name__ == "__main__":
-    # Show the version number of the system
-    print(show_version())
     # Run the Flask app
-    app.run(host='0.0.0.0')
+    app.run()
