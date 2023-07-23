cdf8680ffd6a66d5fdb1bed7aadd897a07c4a233
Arjan Gupta <arjangupta95@gmail.com>
2023-07-22 22:24:23 -0500

    Change rpi hostname variable in pull/push script

--- pull_from_pi.sh
+++ pull_from_pi.sh
@@ -2,11 +2,11 @@
 
 # This function takes in filename as an argument, and gets it via scp from the RPi
 get_file_from_pi() {
-    scp pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/$1 ./$1
+    scp pi@$rpi2:~/workspace/6dof-rpi-deployment/$1 ./$1
 }
 
 get_dir_from_pi() {
-    scp -r pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/$1 .
+    scp -r pi@$rpi2:~/workspace/6dof-rpi-deployment/$1 .
 }
 
 # Get files from the pi
--- push_to_pi.sh
+++ push_to_pi.sh
@@ -2,11 +2,11 @@
 
 # This function takes in filename as an argument, and pushes it via scp to the RPi
 push_file_to_pi() {
-    scp ./$1 pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/$1
+    scp ./$1 pi@$rpi2:~/workspace/6dof-rpi-deployment/$1
 }
 
 push_dir_to_pi() {
-    scp -r ./$1 pi@raspberrypitwo.local:~/workspace/6dof-rpi-deployment/
+    scp -r ./$1 pi@$rpi2:~/workspace/6dof-rpi-deployment/
 }
 
 # Use scp to copy the file to the pi
