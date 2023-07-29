6d1c558bd5d5c8602afc5487066408a64a9046d3
Arjan Gupta <arjangupta95@gmail.com>
2023-07-28 21:25:54 -0500

    Rewrite the ikpy model script to calculate inverse/forward kinematics

--- kinematics/ikpy_model.py
+++ kinematics/ikpy_model.py
@@ -1,15 +1,19 @@
-from ikpy.chain import Chain
-from ikpy.link import Link
-from ikpy.utils import plot
-from mpl_toolkits.mplot3d import Axes3D;
+import ikpy.chain
+import numpy as np
+import ikpy.utils.plot as plot_utils
 
-# Declare links
-link_1 = Link("link_1", length=3)
+# Create the chain
+sixdof_chain = ikpy.chain.Chain.from_urdf_file("kinematics/sixdof_arm.urdf")
 
-# Build the whole chain
-main_chain = Chain(name='main_chain', links=[link_1])
+# Inverse kinematics
+target_position = [6.1, 6.1, 1.1]
+print("The angles of each joints are : ", sixdof_chain.inverse_kinematics(target_position))
+real_frame = sixdof_chain.forward_kinematics(sixdof_chain.inverse_kinematics(target_position))
+print("Computed position vector : %s, original position vector : %s" % (real_frame[:3, 3], target_position))
 
 # Plot
-fig, ax = plot.init_3d_figure()
-main_chain.plot([0] * (len(main_chain)), ax)
-ax.legend()
+import matplotlib.pyplot as plt
+fig, ax = plot_utils.init_3d_figure()
+sixdof_chain.plot(sixdof_chain.inverse_kinematics(target_position), ax, target=target_position)
+plt.xlim(0, 10)
+plt.ylim(0, 10)
